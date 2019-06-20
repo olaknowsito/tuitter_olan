ff3679cfdff69d78518454fc251262778b723e09
ola <olanochristopher@gmail.com>
2019-06-21 01:11:08 +0800

    added db and upload src

--- config/config.php
+++ config/config.php
@@ -6,7 +6,7 @@
 	//displays the date
 	//echo "<span style='color:red;font-weight:bold;'>Date: </span>". date('F j, Y g:i:a  ');
 
-	$con = mysqli_connect("localhost","root","","social");
+	$con = mysqli_connect("xq7t6tasopo9xxbs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","uqe283jjwcy9tsu9","cr0j5i9v4l9os2e6","dphskh4be06hup8z");
 
 	/*return an error*/
 	if(mysqli_connect_errno()){
